64dc7724a51d69403969383b2d07a7ed133ae6f8
Larkin South <160358440+larkin-south@users.noreply.github.com>
2024-05-05 22:15:52 -0500

    Complete through select.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,25 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    until i == self.length
+      yield self[i], i
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    i = 0
+    filter = []
+    until i == self.length
+      if (yield self[i])
+        filter << self[i]
+      end
+      i += 1
+    end
+    filter
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +28,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    i = 0
+    until i == self.length
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
